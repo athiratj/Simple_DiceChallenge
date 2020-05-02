c97b25254e47a82ae7b43e2cc57d75fe98796e3d
athira_tj <athiratjthanaly83@gmail.com>
2020-05-02 13:38:01 +0530

    dice challenge using simple JS.

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelector(".img1");
+
+image1.setAttribute("src", randomDiceImage1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
